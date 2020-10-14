d2ee5dd24b2a13b1642721c7cfba2f98efd4414c
Kirill <kir3811@mail.ru>
2020-10-14 20:46:46 +0300

    Было сложно и интересно

--- public/hw1.4.php
+++ public/hw1.4.php
@@ -2,6 +2,7 @@
 $h1 = "Заголовок";
 $title = "Привет";
 $year = date('Y');
+$menu = ['One', 'two', 'three', ['1', '2', '3']];
 ?>
 
 <head>
@@ -13,5 +14,20 @@ $year = date('Y');
 <body>
 	<h1><?php echo $h1; ?></h1>
 	<?php echo $year; ?>
+	<ul>
+	<?php 
+	foreach($menu as $value) {
+		if (is_array($value)) {
+			echo "<ul>";
+			foreach($value as $intValue) {
+				echo "<li>$intValue</li>";
+			}
+			echo "</ul>";
+		} else {
+			echo "<li>$value</li>";
+		}
+	}     
+	?>
+	</ul>
 </body>
 
