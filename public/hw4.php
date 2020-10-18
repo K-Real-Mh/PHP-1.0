27a206ac240dcc9a6bb2be371d1665e0caf829e2
Kirill <kir3811@mail.ru>
2020-10-18 23:47:50 +0300

    ДЗ №4 готово

---
+++ public/hw4.php
@@ -0,0 +1,20 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
+require_once ROOT_DIR . "engine/draw.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Cache-Control" content="no-cache">
+	<link rel="stylesheet" href="src/layout/styles/style.css">
+	<title>Document</title>
+</head>
+<body>
+	<div class="container">
+		<?php drawImg($imgs) ?>
+	</div>
+</body>
+</html>
